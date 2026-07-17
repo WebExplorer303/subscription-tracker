7da300012998579b39f4040896671ba05d523d3b
Mayuresh Arolkar <arolkarmsa@gmail.com>
2026-07-17 19:24:54 +0530

    v6

--- app/auth/sign-in/[[...sign-in]]/page.tsx
+++ app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,8 @@ import SignInPage from '@/components/sign-in'
 import { ClerkProvider} from '@clerk/nextjs'
 
 export default function SignIn() {
-  return
+  return(
           <ClerkProvider>
               <SignInPage />
-          </ClerkProvider>
+          </ClerkProvider>)
 }

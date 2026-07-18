1b4e36e94f9b40aaad90ab7897cae075a28bd965
Mayuresh Arolkar <arolkarmsa@gmail.com>
2026-07-18 12:01:07 +0530

    vf5

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/navbar";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-<div className="flex flex-col md:flex-row md:h-screen bg-gray-50 dark:bg-gray-900">
+<div className="flex flex-col md:flex-row min-h-screen bg-gray-50 dark:bg-gray-900">
   <Navbar />
   <main className="flex-1 overflow-y-auto p-6 sm:p-10">{children}</main>
 </div>
